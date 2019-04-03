c3203e9c6dad9153835d5e238d085a4bef8225f0
Lyndon <lyndondrach@gmail.com>
2019-04-03 14:17:32 -0700

    ActionBar

--- app/src/main/java/com/example/uniplan2/calendar.java
+++ app/src/main/java/com/example/uniplan2/calendar.java
@@ -1,44 +1,26 @@
 package com.example.uniplan2;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
-
-import com.alamkanak.weekview.MonthLoader;
-import com.alamkanak.weekview.WeekView;
-import com.alamkanak.weekview.WeekViewEvent;
-
-import java.util.List;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class calendar extends AppCompatActivity {
 
-    WeekView mWeekView;
+    Button backBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calender);
 
-        // Get a reference for the week view in the layout.
-        mWeekView = (WeekView) findViewById(R.id.weekView);
-
-        // Set an action when any event is clicked.
-       // mWeekView.setOnEventClickListener(mEventClickListener);
-
-        // The week view has infinite scrolling horizontally. We have to provide the events of a
-        // month every time the month changes on the week view.
-       // mWeekView.setMonthChangeListener(mMonthChangeListener);
-
-        // Set long press listener for events.
-       // mWeekView.setEventLongPressListener(mEventLongPressListener);
-
-        MonthLoader.MonthChangeListener mMonthChangeListener = new MonthLoader.MonthChangeListener() {
+        backBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public List<WeekViewEvent> onMonthChange(int newYear, int newMonth) {
-                // Populate the week view with some events.
-               // List<WeekViewEvent> events = getEvents(newYear, newMonth);
-                //return events;
-                return null;
+            public void onClick(View v) {
+                Intent i = new Intent(getBaseContext(), MainActivity.class);
+                startActivity(i);
             }
-        };
+        });
     }
 }
