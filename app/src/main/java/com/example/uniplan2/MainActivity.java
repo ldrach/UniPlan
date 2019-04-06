3062ff1dcc43901441cf740acb8ee6804d19643a
Lyndon <lyndondrach@gmail.com>
2019-04-06 09:34:51 -0700

    AddClass Intent set up to return all fields

--- app/src/main/java/com/example/uniplan2/MainActivity.java
+++ app/src/main/java/com/example/uniplan2/MainActivity.java
@@ -51,6 +51,7 @@ public class MainActivity extends AppCompatActivity {
     public String[] taskDisplay;
 
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
